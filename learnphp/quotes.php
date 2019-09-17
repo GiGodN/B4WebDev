c5b64753e6db63acf2f8f3de114c217b713a9d79
GiGodN <TheCaptonNoahJ@gmail.com>
2019-09-17 13:45:01 -0600

    finilized the final product

--- learnphp/quotes.php
+++ learnphp/quotes.php
@@ -17,8 +17,13 @@
 
         $total = number_format($total, 2);
 
-        print '<p>You are purchasing <strong>' . $quantity . '</strong> widget(s) at a cost of <strong>' . $price . 
-        '</strong> each. With tax, the total comes to <strong>' . $total . '</strong>.</p>';
+        print "<h3> Using double quotation marks:</h3>";
+        print "<p>You are purchasing <strong> $quantity </strong> widget(s) at a cost of <strong> \$$price 
+        </strong> each. With tax, the total comes to <strong> \$$total </strong>.</p>";
+
+        print "<h3> Using single quotation marks:</h3>";
+        print '<p>You are purchasing <strong> $quantity </strong> widget(s) at a cost of <strong> \$$price 
+        </strong> each. With tax, the total comes to <strong> \$$total </strong>.</p>'
     ?>
 </body>
 </html>
